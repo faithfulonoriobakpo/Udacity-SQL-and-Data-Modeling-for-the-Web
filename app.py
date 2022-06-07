ac61a17c54e4b8e4adc9a3086382cc10027976a0
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-06-07 11:55:40 +0100

    Implement and Update Models

--- app.py
+++ app.py
@@ -39,6 +39,12 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='venues', lazy=False, cascade='all, delete-orphan')
+    genres = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
 
 class Artist(db.Model):
@@ -52,8 +58,12 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(500))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    shows = db.relationship('Show', backref='artists', lazy=False, cascade='all, delete-orphan')
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Show(db.Model):
     __tablename__ = 'Show'
@@ -63,9 +73,8 @@ class Show(db.Model):
     venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __repr__(self):
-        return f'<show id {self.id} artist_id {self.artist_id} venue_id {self.venue_id} start_time {self.start_time}'
 
+#db.create_all()
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
