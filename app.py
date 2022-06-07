651db5381786e8daa0267447862c1a7424bedd73
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-06-07 10:50:26 +0100

    Created a Show Model

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ class Venue(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -59,7 +58,16 @@ class Artist(db.Model):
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<show id {self.id} artist_id {self.artist_id} venue_id {self.venue_id} start_time {self.start_time}'
 
 #----------------------------------------------------------------------------#
 # Filters.
